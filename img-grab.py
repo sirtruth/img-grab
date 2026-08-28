ed9bdd90c999ce030899e401114eba72c0a35aca
sirtruth <sirtruthsecurity@gmail.com>
2026-08-28 04:01:58 -0700

    Update img-grab.py

--- img-grab.py
+++ img-grab.py
@@ -15,7 +15,10 @@ def get_search_query_name(url):
 
 def scrape_images(url, limit=25):
     print(f"[+] Fetching images from: {url}")
-    headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+}
+
     
     try:
         response = requests.get(url, headers=headers, timeout=10)
